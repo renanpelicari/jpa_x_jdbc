6f2e6f91a4e5c219a5aa2d214b558d323fcadb76
Renan Pelicari Rodrigues <renanpr@lnxcit012084.cit>
2017-11-26 15:04:53 -0200

    Initial version

---
+++ README.md
@@ -0,0 +1,1 @@
+# jpa_x_jdbc
---
+++ src/main/java/poc/springboot/jpaxjdbc/JpaXJdbcApplication.java
@@ -0,0 +1,12 @@
+package poc.springboot.jpaxjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaXJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaXJdbcApplication.class, args);
+	}
+}

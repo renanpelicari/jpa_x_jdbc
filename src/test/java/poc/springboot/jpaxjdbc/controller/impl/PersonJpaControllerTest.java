0e45ef41373c1fb51d389c66221f36048dbcc08b
Renan Pelicari Rodrigues <renanpr@lnxcit012084.cit>
2017-12-08 02:06:51 -0200

    Added some test cases

--- src/test/java/poc/springboot/jpaxjdbc/controller/impl/PersonJpaControllerTest.java
+++ src/test/java/poc/springboot/jpaxjdbc/controller/impl/PersonJpaControllerTest.java
@@ -1,6 +1,7 @@
 package poc.springboot.jpaxjdbc.controller.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,11 @@ import poc.springboot.jpaxjdbc.service.impl.PersonJpaService;
 import poc.springboot.jpaxjdbc.vo.request.PersonRequestVo;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -26,6 +29,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(value = PersonJpaController.class, secure = false)
 public class PersonJpaControllerTest {
 
+    //TODO: Missing findAll tests and some cases of Delete. Also missing test with not exists ID
+    // to perform: mock return, and check endpoint behavior
     @Autowired
     private MockMvc mvc;
 
@@ -37,34 +42,146 @@ public class PersonJpaControllerTest {
 
     private final String ENDPOINT_URL = "/api/person/jpa";
 
+    private PersonRequestVo personRequestVo;
+
+    private final int PERSON_ID = 1;
+
+    @Before
+    public void setUp() {
+        personRequestVo = PersonRequestVoFixture.getPersonRequestVo();
+    }
+
     @Test
     public void testRegisterSuccess() throws Exception {
-        final PersonRequestVo personRequestVo = PersonRequestVoFixture.getPersonRequestVo();
-
         mvc.perform(post(ENDPOINT_URL)
             .contentType(MediaType.APPLICATION_JSON)
             .content(mapper.writeValueAsString(personRequestVo))
             .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk());
-
+            .andExpect(status().isOk())
+            .andExpect(content().bytes(new byte[0]));
         verify(personJpaService).register(personRequestVo);
     }
 
     @Test
-    public void testRegisterMediaTypeNotAccept() throws Exception {
-        final PersonRequestVo personRequestVo = PersonRequestVoFixture.getPersonRequestVo();
+    public void testRegisterErrorMissingAge() throws Exception {
+        mvc.perform(post(ENDPOINT_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(PersonRequestVo.builder().fullName("Renan Rodrigues").build()))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
+        verify(personJpaService, never()).register(any());
+    }
 
+    @Test
+    public void testRegisterErrorMissingName() throws Exception {
+        mvc.perform(post(ENDPOINT_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(PersonRequestVo.builder().age(87).build()))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
+        verify(personJpaService, never()).register(any());
+    }
+
+    @Test
+    public void testRegisterErrorMediaTypeNotAcceptable() throws Exception {
         mvc.perform(post(ENDPOINT_URL)
             .contentType(MediaType.APPLICATION_JSON)
             .content(mapper.writeValueAsString(personRequestVo))
             .accept(MediaType.APPLICATION_XML_VALUE))
-            .andExpect(status().is4xxClientError());
+            .andExpect(status().isNotAcceptable());
+        verify(personJpaService, never()).register(any());
+    }
 
+    @Test
+    public void testRegisterErrorMethodGetNotAcceptable() throws Exception {
+        mvc.perform(get(ENDPOINT_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(personRequestVo))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isMethodNotAllowed());
+        verify(personJpaService, never()).register(any());
+    }
+
+    @Test
+    public void testRegisterErrorMethodDeleteNotAcceptable() throws Exception {
+        mvc.perform(delete(ENDPOINT_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(personRequestVo))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isMethodNotAllowed());
+        verify(personJpaService, never()).register(any());
+    }
+
+    @Test
+    public void testRegisterErrorMethodPutNotAcceptable() throws Exception {
+        mvc.perform(put(ENDPOINT_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(personRequestVo))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isMethodNotAllowed());
         verify(personJpaService, never()).register(any());
     }
 
     @Test
     public void testUpdateSuccess() throws Exception {
+        mvc.perform(put(ENDPOINT_URL + "/" + PERSON_ID)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(personRequestVo))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().bytes(new byte[0]));
+        verify(personJpaService, never()).register(personRequestVo);
+        verify(personJpaService).update(PERSON_ID, personRequestVo);
+    }
+
+    @Test
+    public void testUpdateErrorMissingAge() throws Exception {
+        mvc.perform(put(ENDPOINT_URL + "/" + PERSON_ID)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(PersonRequestVo.builder().fullName("Renan Rodrigues").build()))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
+        verify(personJpaService, never()).update(PERSON_ID, personRequestVo);
+    }
+
+    @Test
+    public void testUpdateErrorMissingName() throws Exception {
+        mvc.perform(put(ENDPOINT_URL + "/" + PERSON_ID)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(PersonRequestVo.builder().age(87).build()))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
+        verify(personJpaService, never()).update(PERSON_ID, personRequestVo);
+    }
+
+    @Test
+    public void testUpdateErrorMediaTypeNotAcceptable() throws Exception {
+        mvc.perform(put(ENDPOINT_URL + "/" + PERSON_ID)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(personRequestVo))
+            .accept(MediaType.APPLICATION_XML_VALUE))
+            .andExpect(status().isNotAcceptable());
+        verify(personJpaService, never()).update(PERSON_ID, personRequestVo);
+    }
+
+    @Test
+    public void testUpdateErrorMethodGetNotAcceptable() throws Exception {
+        mvc.perform(get(ENDPOINT_URL + "/" + PERSON_ID)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(personRequestVo))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isMethodNotAllowed());
+        verify(personJpaService, never()).update(PERSON_ID, personRequestVo);
+    }
+
+    @Test
+    public void testUpdateErrorMethodPostNotAcceptable() throws Exception {
+        mvc.perform(post(ENDPOINT_URL + "/" + PERSON_ID)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(personRequestVo))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isMethodNotAllowed());
+        verify(personJpaService, never()).update(PERSON_ID, personRequestVo);
     }
 
     @Test
@@ -73,7 +190,14 @@ public class PersonJpaControllerTest {
 
     @Test
     public void testDeleteSuccess() throws Exception {
+        mvc.perform(delete(ENDPOINT_URL + "/" + PERSON_ID)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().bytes(new byte[0]));
+        verify(personJpaService, never()).register(any());
+        verify(personJpaService, never()).update(anyInt(), any());
+        verify(personJpaService).delete(PERSON_ID);
     }
-
 
 }
